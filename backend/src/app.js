7d87089a1fcc8d17f9961b5aa00436d86e5c0061
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-10-03 20:12:27 -0300

    chore: adiciona configuração de cors no app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors")
 const router = require("./routers/loginRouter");
 const app = express();
 
+app.use(cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"]
+}))
 app.use(express.json());
 app.use(router);
 
