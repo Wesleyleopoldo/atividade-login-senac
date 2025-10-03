911cc24a99f8288671e687098ff0ee860238f0d9
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-10-03 20:13:48 -0300

    feat: adiciona configuração de cookies para autenticação com token jwt

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,7 +2,15 @@ const userService = require("../services/userServices");
 
 const login = async (request, response) => {
     const token = await userService.login(request.body);
-    return response.status(200).json(token);
+    response.cookie(
+        "token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "Lax"
+        }
+    )
+
+    return response.status(200).json({ message: "Sucesso" });
 }
 
 const createUser = async (request, response) => {
