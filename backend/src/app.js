80d59d5c5b0471e1d1c9e5d65fd0aa835fb593d7
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-10-04 00:00:10 -0300

    fix: adiciona middleware de erro no app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
 const router = require("./routers/loginRouter");
 const app = express();
 
 app.use(cors({
     origin: ["http://127.0.0.1:5500", "http://localhost:5500"]
-}))
+}));
+
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 module.exports = app;
